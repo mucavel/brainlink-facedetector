1ed44dd85af7c15178577a18ab6e0d53b421921b
mucavel <salvadormuca7@gmail.com>
2022-06-30 22:16:15 +0200

    Added Choose img button

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React from "react";
 import './css/InputForm.css'
 
 const InputForm = ({onInputChange}) =>{
--- src/components/InputForm/UploadButton.jsx
+++ src/components/InputForm/UploadButton.jsx
@@ -5,7 +5,7 @@ const UploadButton = ({onFileUpload}) => {
     const file = useRef(); 
     return(
         <div className="upload-btn-container">
-            <input type="file" id="file" ref={file}  onChange={() => {
+            <input type="file" id="file" accept="images/*" ref={file}  onChange={() => {
                 let reader = new FileReader();
                 reader.readAsDataURL(file.current.files[0]);
                 reader.onload = () => {
